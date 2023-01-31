dd3990752e06c04c9b5f42a4d314d9779fcc0ea1
Acid-Division <96349900+Charli420@users.noreply.github.com>
2023-01-31 17:26:18 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: chabrune <charlesbrunet51220@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 16:50:14 by chabrune          #+#    #+#             */
-/*   Updated: 2023/01/31 16:53:26 by chabrune         ###   ########.fr       */
+/*   Updated: 2023/01/31 17:19:58 by chabrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,45 +47,8 @@ int main(int argc, char **argv)
     if (ft_check_double(&a) == 1)
         return(write(2, "Error\n", 6));
     ft_find_index(&a);
-    ft_printf("A = \n");
-    ft_printf("%d\n", a.val[0].value);
-    ft_printf("%d\n", a.val[1].value);
-    ft_printf("%d\n", a.val[2].value);
-    ft_printf("%d\n", a.val[3].value);
-    ft_printf("%d\n", a.val[4].value);
-    ft_printf("%d\n\n", a.val[5].value);
-    ft_printf("index = \n");
-    ft_printf("%d\n", a.val[0].index);
-    ft_printf("%d\n", a.val[1].index);
-    ft_printf("%d\n", a.val[2].index);
-    ft_printf("%d\n", a.val[3].index);
-    ft_printf("%d\n", a.val[4].index);
-    ft_printf("%d\n\n", a.val[5].index);
-    swap_a(&a);
     push_b(&a, &b);
-    push_b(&a, &b);
-    push_b(&a, &b);
-    rotate_a(&a);
-    rotate_b(&b);
-    reverse_rotate_a(&a);
-    reverse_rotate_b(&b);
-    swap_a(&a);
-    push_a(&a, &b);
-    push_a(&a, &b);
-    push_a(&a, &b);
-    ft_printf("A = \n");
-    ft_printf("%d\n", a.val[0].value);
-    ft_printf("%d\n", a.val[1].value);
-    ft_printf("%d\n", a.val[2].value);
-    ft_printf("%d\n", a.val[3].value);
-    ft_printf("%d\n", a.val[4].value);
-    ft_printf("%d\n\n", a.val[5].value);
-    ft_printf("B = \n");
-    ft_printf("%d\n", b.val[0].value);
-    ft_printf("%d\n", b.val[1].value);
-    ft_printf("%d\n", b.val[2].value);
-    ft_printf("%d\n", b.val[3].value);
-    ft_printf("%d\n", b.val[4].value);
-    ft_printf("%d\n\n", b.val[5].value);
+    free(&a);
+    free(&b);
     return(0);
 }
