1b2830f22e50027685adc87e770e1cd74d9f0ce2
Acid-Division <96349900+Charli420@users.noreply.github.com>
2023-02-10 09:26:09 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: chabrune <charlesbrunet51220@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 16:50:14 by chabrune          #+#    #+#             */
-/*   Updated: 2023/02/04 18:44:34 by chabrune         ###   ########.fr       */
+/*   Updated: 2023/02/08 23:14:02 by chabrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,50 +31,25 @@ void    ft_find_index(t_stack *a)
         }
         i++;
     }
-    printf("%d\n", a->val[0].index);
-    printf("%d\n", a->val[1].index);
-    printf("%d\n", a->val[2].index);
-    printf("%d\n", a->val[3].index);
-    printf("%d\n", a->val[4].index);
-
 }
-
-// void    sort_emma(t_stack *a, t_stack *b)
-// {
-//     int i;
-//     int emma_len;
-
-//     emma_len = a->len;
-//     i = 0;
-//     while(i < emma_len)
-//     {
-//         if (a->val[0].index > emma_len / 2)
-//             push_b(a, b);
-//         else
-//             rotate_a(a);
-//         i++;
-//     }
-// }
 
 void    ola_pepito(t_stack *a, t_stack *b)
 {
     int i;
     int j;
-    int k;
     int max_index;
     int max_bits;
     int len;
 
     len = a->len;
-    j = 0;
     i = 0;
-    k = 0;
     max_index = a->len - 1;
     max_bits = 0;
     while((max_index >> max_bits) != 0)
         max_bits++;
     while(max_bits > i)
     {
+        j = 0;
         while(j < len)
         {
             if ((a->val[0].index >> i & 1) == 1)
@@ -89,20 +64,49 @@ void    ola_pepito(t_stack *a, t_stack *b)
     }
 }
 
+int    ft_already_sorted(t_stack *a)
+{
+    int i;
+    int fopa;
+
+    fopa = 0;
+    i = 0;
+    if(a->val[i].index > a->val[i + 1].index)
+        fopa++;
+    i++;
+    while(i < a->len)
+    {
+        if(a->val[i].index < a->val[i - 1].index)
+            fopa++;
+        i++;
+    }
+    return(fopa);
+}
+
+// void    ft_sort_stack_three(t_stack *a, t_stack *b)
+// {
+// }
+
 int main(int argc, char **argv)
 {
-    t_stack a;
-    t_stack b;
+    t_stack *a;
+    t_stack *b;
+    a = malloc(sizeof(t_stack));
+    b = malloc(sizeof(t_stack));
     if(argc == 2)
     {
-        if (ft_fill_split(argv[1], &a, &b) == 1)
+        if (ft_fill_split(argv[1], a, b) == 1)
             return(write(2, "Error\n", 6));
     }
     else
-        ft_fill_arg(argv, &a, &b, argc);
-    if (ft_check_double(&a) == 1)
+        ft_fill_arg(argv, a, b, argc);
+    if (ft_check_double(a) == 1)
         return(write(2, "Error\n", 6));
-    ft_find_index(&a);
-    ola_pepito(&a, &b);
+    ft_find_index(a);
+    if (ft_already_sorted(a)  == 0)
+        return(write(1, "Already sorted", 15));
+    ola_pepito(a, b);
+    free(a);
+    free(b);
     return(0);
 }
